26921dce3afc59205cee151db8de5db8f2ca5492
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-08-27 19:45:43 +0600

    rootPgae handle module fixed

--- src/routes/common-router.ts
+++ src/routes/common-router.ts
@@ -1,4 +1,4 @@
-import { rootRouteHandler } from '@controllers/root-router';
+import { rootRouteHandler } from '@controllers/root-page';
 import {Router} from 'express';
 import configRouter from 'src/utilities/config-router';
 
